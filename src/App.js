134451bd700aeacba91a660c22f034e7f6b5c66d
yapsbjj <ya.ramdane@laposte.net>
2023-12-21 21:41:11 +0100

    add cv react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Realisations from './pages/Realisations';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import './pages/styles/components/styles.scss';
+import MentionsLegales from "./pages/MentionsLegales";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/realisations" element={<Realisations />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/mentionsLegales" element={<MentionsLegales />} />
+
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
+
